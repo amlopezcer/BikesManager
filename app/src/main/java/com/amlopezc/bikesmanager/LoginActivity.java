544caa222fb905b4d5b0eb7db6d9c0234a86369d
Alejandro M. Lopez Cerezo <am.lopezcer@gmail.com>
2016-04-17 13:36:29 +0200

    Pantalla de inicio de sesión mejorada
    Retocada la pantalla de inicio de sesión, incluyendo strings en el strings.xml, dimensiones en el dimens.xml etc. Se incluye la recogida automática del número de versión.

--- app/src/main/java/com/amlopezc/bikesmanager/LoginActivity.java
+++ app/src/main/java/com/amlopezc/bikesmanager/LoginActivity.java
@@ -1,24 +1,29 @@
 package com.amlopezc.bikesmanager;
 
-
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
-import android.view.WindowManager;
+import android.util.Log;
+import android.widget.TextView;
+
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-        //Remove title bar
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //Remove notification bar
-        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView textViewVersion = (TextView) findViewById(R.id.textView_version_text);
+
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String version = pInfo.versionName;
+            textViewVersion.setText(version);
+        } catch (PackageManager.NameNotFoundException nnfe) {
+            Log.e(getClass().getCanonicalName(), nnfe.getLocalizedMessage(), nnfe);
+        }
 
     }
 }
